45413085336f58cab3661ca8854ad6e236f6000d
dimitryak34 <dkozylbashev@gmail.com>
2025-10-12 23:08:02 +0300

    added stack init

--- lib-stack/stack.h
+++ lib-stack/stack.h
@@ -1,8 +1,40 @@
 #include <iostream>
+#include "../lib_TVector/TVector.h"
 
-class Stack {
+template <class T>
+class Stack:  {
+	size_t _top;
+	size_t _size;
+	T* _data;
 public:
-	void stack() {
-		std::cout << "i am stack" << std::endl;
-	}
+	Stack() : _top(0), _size(0), _data(nullptr){}
+	Stack(const T& data, size_t size);
+	Stack(std::initializer_list<T> data, size_t size);
+
+	void push(const T& val);
+	void pop();
+	void clear() noexcept;
+	T top();
+	bool isEmpty();
+	bool isFull();
+
 };
+
+template <class T>
+Stack<T>::Stack(const T& data, size_t size) {
+	_size = size;
+	_top = size - 1;
+	_data = new T[_size];
+	for (int i = 0; i < _size; i++) {
+		_data[i] = data[i];
+	}
+}
+template <class T>
+Stack<T>::Stack(std::initializer_list<T> data, size_t size) {
+	_size = size;
+	_top = _size - 1;
+	_data = new T[_size];
+	for (int i = 0; i < size; i++) {
+		_data[i] = data[i];
+	}
+}
