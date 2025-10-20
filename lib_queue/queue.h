074de860111957c6e27bb768c2801a741b8de47d
dimitryak34 <dkozylbashev@gmail.com>
2025-10-20 20:51:15 +0300

    added methods

--- lib_queue/queue.h
+++ lib_queue/queue.h
@@ -7,7 +7,7 @@ class TQueue {
 	int _head;
 	T* _data;
 public:
-	TQueue(): _data(nullptr), _head(0), _size(0), _count(0){}
+	TQueue(): _data(nullptr), _head(-1), _size(0), _count(0){}
 	TQueue(std::initializer_list<T> data);
 	TQueue(size_t size);
 
@@ -18,6 +18,11 @@ public:
 	void pop();
 	bool isEmpty();
 	bool isFull();
+
+	size_t size() { return _size; }
+	size_t count() { return _count; }
+	int head() { return _head; }
+
 };
 
 template <class T>
@@ -34,8 +39,11 @@ template <class T>
 TQueue<T>::TQueue(size_t size) {
 	_data = new T[size];
 	_size = size;
+	for (int i = 0; i < _size; i++) {
+		_data[i] = 0;
+	}
 	_head = _size - 1;
-	_count = 0;
+	_count = _size;
 }
 
 
@@ -58,4 +66,17 @@ void TQueue<T>::pop() {
 	}
 	_count--;
 	_head = (++_head) % _size;
+}
+
+template <class T>
+bool TQueue<T>::isFull() {
+	if (_count == _size) {
+		return true;
+	}
+	return false;
+}
+template <class T>
+bool TQueue<T>::isEmpty() {
+	if (_count == 0) { return true; }
+	return false;
 }
--- tests/test_queue.cpp
+++ tests/test_queue.cpp
@@ -5,3 +5,11 @@ TEST(testQueue, can_init_queue) {
 	ASSERT_NO_THROW(TQueue({1, 2, 3, 4}));
 }
 
+TEST(testQueue, test_default_constr) {
+	TQueue<int> q1;
+
+	EXPECT_EQ(q1.size(), 0);
+	EXPECT_EQ(q1.count(), 0);
+	EXPECT_EQ(q1.head(), -1);
+
+}
